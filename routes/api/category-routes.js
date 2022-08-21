d7b84a80949bbb836f99779764832762248047d4
Artur0705 <artlil420@gmail.com>
2022-08-21 20:59:36 +1000

    route created for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,68 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", async (req, res) => {
+  const category = await Category.findAll({
+    include: [
+      {
+        model: Product,
+        required: true,
+      },
+    ],
+  });
+
+  return res.json(category);
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  const category = await Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  });
+  return res.json(category);
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  if (!req.body["category_name"]) {
+    return res.json({
+      message: "field is required",
+    });
+  }
+
+  const category = await Category.create(await req.body);
+  return res.json(category);
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  if (!req.body["category_name"]) {
+    return res.json({
+      message: "field is required",
+    });
+  }
+
+  const category = await Category.update(await req.body, {
+    where: { id: req.params.id },
+  });
+  return res.json(category);
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+router.delete("/:id", async (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
 
+  return res.json({
+    message: "Deleted category " + req.params.id,
+  });
+});
 module.exports = router;
